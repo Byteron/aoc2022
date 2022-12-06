0f5cbd692b2e7986fb3d083932f56abd136709b1
Aaron Winter <wintera@Aarons-MacBook-Pro.local>
2022-12-06 08:09:40 +0100

    day 5 part 2

--- aoc5/src/main.zig
+++ aoc5/src/main.zig
@@ -5,7 +5,7 @@ const ArrayList = std.ArrayList;
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
 
-const test_runs = 100;
+const test_runs = 1;
 
 const Stack = struct {
     array: [100]u8 = [_]u8{0} ** 100,
@@ -36,9 +36,7 @@ const Stack = struct {
     }
 };
 
-pub fn solve1(input: []const u8) !u32 {
-    var total_score: u32 = 0;
-    
+pub fn solve1(input: []const u8) !void {
     var lines = std.mem.split(u8, input, "\n\n");
     
     const table = lines.next().?;
@@ -91,47 +89,96 @@ pub fn solve1(input: []const u8) !u32 {
     for (stacks) |stack| {
         std.debug.print("{c}\n", .{ stack.peek() });
     }
-    
-    return total_score;
 }
 
-pub fn solve2(input: []const u8) !u32 {
-    var total_score: u32 = 0;
+pub fn solve2(input: []const u8) !void {
+    var lines = std.mem.split(u8, input, "\n\n");
     
-    var lines = std.mem.tokenize(u8, input, "\n\r");
+    const table = lines.next().?;
+    const moves = lines.next().?;
     
-    while (lines.next()) |line| {
-        std.debug.print("{s}\n", .{ line });
+    var stacks = [_]Stack{Stack{}} ** 9;
+    
+    var rows = std.mem.split(u8, table, "\n");
+    
+    while(rows.next()) |row| {
+        if (std.mem.eql(u8, row[1..2], "1")) break;
+        
+        var index: u8 = 0;
+        while (index < 9)
+        {
+            const char_index = index * 4 + 1;
+            const char = row[char_index];
+            if (std.mem.eql(u8, row[char_index..char_index+1], " ")) {
+                index += 1;
+                continue;   
+            }
+            stacks[index].push_front(char);
+            index += 1;
+        }
     }
     
-    return total_score;
+    var commands = std.mem.split(u8, moves, "\n");
+    
+    while (commands.next()) |command| {
+        if (std.mem.eql(u8, command, "")) break;
+        
+        var split = std.mem.split(u8, command, " ");
+        
+        _ = split.next();
+        const count = try std.fmt.parseInt(u8, split.next().?, 10);
+        _ = split.next();
+        const from = try std.fmt.parseInt(u8, split.next().?, 10);
+        _ = split.next();
+        const to = try std.fmt.parseInt(u8, split.next().?, 10);
+        
+        
+        var temp_stack = Stack{};
+        var index : u8 = 0;
+        while (index < count)
+        {
+            const value = stacks[from - 1].pop();
+            temp_stack.push(value);
+            index += 1;
+        }
+        
+        index = 0;
+        while (index < count)
+        {
+            const value = temp_stack.pop();
+            stacks[to - 1].push(value);
+            index += 1;
+        }
+    }
+    
+    for (stacks) |stack| {
+        std.debug.print("{c}\n", .{ stack.peek() });
+    }
 }
 
-pub fn bench(comptime solve: fn ([] const u8) anyerror!u32) !void {
+pub fn bench(comptime solve: fn ([] const u8) anyerror!void) !void {
     const input = @embedFile("input.txt");
     var counter: u32 = test_runs;
     var timer = try Timer.start();
     var time: u64 = 0;
-    var score: u32 = 0;
     
     while (counter > 0) {
-        score = try solve(input);
+        try solve(input);
         time += timer.lap();
         counter -= 1;
     }
     
-    defer _ = gpa.deinit();
-    
-    std.debug.print("Score: {d}\n", .{score});
     std.debug.print("ns: {}\n", .{time / test_runs});
 }
 
 pub fn main() !void {
     try bench(solve1);
-    // try bench(solve2);
+    try bench(solve2);
+    defer _ = gpa.deinit();
 }
 
 test {
     try bench(solve1);
-    // try bench(solve2);
+    try bench(solve2);
+    defer _ = gpa.deinit();
 }
