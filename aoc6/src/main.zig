4ac4efc325501231c892452431833ac6d9af92dd
Aaron Winter <wintera@Aarons-MacBook-Pro.local>
2022-12-06 10:25:00 +0100

    day 6 part 2

---
+++ aoc6/src/main.zig
@@ -0,0 +1,58 @@
+const std = @import("std");
+const Timer = std.time.Timer;
+
+pub fn solve(input: []const u8, count: u8) !u32 {
+    var lines = std.mem.tokenize(u8, input, "\n\r");
+    
+    var line = lines.next().?;
+    
+    var index: u32 = 0;
+    var bits: u64 = 0;
+    var prev_count: u32 = 0;
+    var bit_count: u32 = 0;
+    while (bit_count < count and index < line.len) {
+        const c = line[index];
+        const bit: u64 = @as(u64, 1) << @intCast(u6, (c - 'a'));
+        bits |= bit;
+        bit_count = @popCount(bits);
+        // std.debug.print("Index: {d}, C: {c} Count: {d}, Bit: {b}, Bits: {b}\n", .{index, c, @popCount(bits), bit, bits});
+        if (bit_count == prev_count) {
+            index -= prev_count;
+            bits = 0;
+            prev_count = 0;
+        }
+        else {
+            prev_count = bit_count;
+        }
+        index += 1;
+    }
+
+    return index;
+}
+
+pub fn bench(comptime solvefn: fn ([] const u8, count: u8) anyerror!u32, count: u8) !void {
+    const input = @embedFile("input.txt");
+    var counter: u32 = 1000;
+    var timer = try Timer.start();
+    var time: u64 = 0;
+    var score: u32 = 0;
+    
+    while (counter > 0) {
+        score = try solvefn(input, count);
+        time += timer.lap();
+        counter -= 1;
+    }
+
+    std.debug.print("Score: {d}\n", .{score});
+    std.debug.print("ns: {}\n", .{time / 1000});
+}
+
+pub fn main() !void {
+    try bench(solve, 4);
+    try bench(solve, 14);
+}
+
+test {
+    try bench(solve, 4);
+    try bench(solve, 14);
+}
